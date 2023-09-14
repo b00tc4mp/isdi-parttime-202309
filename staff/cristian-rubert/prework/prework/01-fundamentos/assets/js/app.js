05124978808d5f243c0ab26fc8a9892949815d40
cristianrubert1 <alurub8913@ieselcaminas.org>
2023-09-14 21:35:25 +0200

    add my prework file

---
+++ staff/cristian-rubert/prework/prework/01-fundamentos/assets/js/app.js
@@ -0,0 +1,22 @@
+
+// console.log('Hola Mundo');
+
+let a = 10, 
+    b = 20, 
+    c = 'Hola ', 
+    d = 'Spiderman', 
+    total = a + b; 
+
+const saludo = c + d;
+
+// console.log('Valor total = ' + total)
+// console.log('Valor de a = ' + a)
+// console.log('Valor de b = ' + b)
+// console.log('Valor de c = ' + c)
+// console.log('Valor de d = ' + d)
+
+c = 'Hola de nuevo';
+
+
+// console.table({a, b, c, d, total});
+let miNombnre = 'Cristian';
