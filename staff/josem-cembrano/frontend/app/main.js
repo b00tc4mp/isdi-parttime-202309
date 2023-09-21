00de0b0d2ca3ea34ed432f532e045bc3fa0d2d85
josem-cembrano <isdichema@gmail.com>
2023-09-21 21:40:38 +0200

    change app files

--- staff/josem-cembrano/frontend/app/main.js
+++ staff/josem-cembrano/frontend/app/main.js
@@ -1,1 +1,9 @@
-document.getElementById('register').style.display = 'none'
+document.getElementById('register').style.
+display = 'none'
+
+var loginView = document.getElementById
+('login')
+
+var loginRegisterLink = loginView.
+querySelector('a')
+
