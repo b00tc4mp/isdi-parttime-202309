267ccaf84cdd4e679ce30d02dc2b693ffce9a71c
Isabel Domínguez Sánchez <134840727+IsaDScmd@users.noreply.github.com>
2023-09-21 21:36:07 +0200

    add login and register user

--- staff/isabel-dominguez/frontend/app/main.js
+++ staff/isabel-dominguez/frontend/app/main.js
@@ -1,1 +1,43 @@
 document.getElementById('register').style.display = 'none';
+
+var registerView = document.getElementById('register');
+var loginView = document.getElementById('login');
+var registerLoginLink = registerView.querySelector('a');
+var loginRegisterLink = loginView.querySelector('a');
+var users = []
+
+registerLoginLink.onclick = function (event) {
+    event.preventDefault();
+    registerView.style.display = 'none';
+    loginView.style.display = 'block';
+};
+
+loginRegisterLink.onclick = function (event) {
+    event.preventDefault();
+    loginView.style.display = 'none';
+    registerView.style.display = 'block';
+};
+
+
+var registerForm = document.querySelector('#tuFormularioID');
+
+registerForm.onsubmit = function (event) {
+    event.preventDefault();
+
+    var name = registerForm.querySelector('#name').value;
+    var email = registerForm.querySelector('#email').value;
+    var password = registerForm.querySelector('#password').value;
+
+    var user = {};
+
+    user.name = name;
+    user.email = email;
+    user.password = password;
+
+    users.push(user);
+
+    registerView.style.display = 'none';
+    loginView.style.display = 'block';
+}
+
+
