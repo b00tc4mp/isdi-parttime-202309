5c208b564f4e15ea6b9bd75ce53775ca9a07d896
Belén Ivars <belen@MacBook-MacBook-Pro-de-Belen.local>
2023-09-23 01:31:09 +0200

    restart app base code process

---
+++ staff/belen-ivars/frontend/app/main.js
@@ -0,0 +1,16 @@
+document.getElementById('register').style.display = 'none'
+
+
+
+
+
+
+
+
+
+/*/document.getElementById('register').registerView.style.display = 'none'
+
+var loginView = document.getElementById('login')
+
+var loginRegisterLink = loginView.
+querySelector('a')*/
