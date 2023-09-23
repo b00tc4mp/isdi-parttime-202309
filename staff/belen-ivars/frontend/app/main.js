4f72f349ce8b64b309fbe1762f0c819ada8c74d7
Belén Ivars <belen@MacBook-MacBook-Pro-de-Belen.local>
2023-09-23 17:16:08 +0200

    add changes

--- staff/belen-ivars/frontend/app/main.js
+++ staff/belen-ivars/frontend/app/main.js
@@ -8,9 +8,3 @@ document.getElementById('register').style.display = 'none'
 
 
 
-/*/document.getElementById('register').registerView.style.display = 'none'
-
-var loginView = document.getElementById('login')
-
-var loginRegisterLink = loginView.
-querySelector('a')*/
