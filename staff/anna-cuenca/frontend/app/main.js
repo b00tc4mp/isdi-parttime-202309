2cf35c640376abcc4da38ea206843ba0369dfc02
Anna Cuenca <acuencabo@gmail.com>
2023-09-25 13:03:26 +0200

    change register funcionality

--- staff/anna-cuenca/frontend/app/main.js
+++ staff/anna-cuenca/frontend/app/main.js
@@ -11,6 +11,9 @@ var users = []
 var userFalse = {}
 
 var userExists = false;
+
+var nameLoginUser
+var nameRegisterUser
     
 
 
@@ -62,33 +65,35 @@ registerForm.onsubmit = function(event){
     var user = {}
     
 
-
-
     user.name = name
     user.email = email
     user.password = password
 
-    //var found = false;
+    var found = false;
     for(var i = 0; i < users.length; i++){
         if (users[i].email == user.email){
-            //found = true;
-            console.log('ERROR this mail is already used, please login')
-            nameInput.value = ''
-            emailInput.value = ''
-            passwordInput.value = ''
-            registerView.style.display = 'none'
-            loginView.style.display = 'block'
-            break
+            found = true;
+            
+            
         } else {
-            //found = false;
-            console.log(`Thanks for register, ${user.name}`)
+            found = false;
+            nameRegisterUser = users[i].name
+        }
+    }
+
+    if (found == true){
+        alert('ERROR this mail is already used, please login');
+        nameInput.value = ''
+        emailInput.value = ''
+        passwordInput.value = ''
+        registerView.style.display = 'none'
+        loginView.style.display = 'block'
+    } else {
+        alert(`Thanks for register, please login`);
             users.push(user)
             registerView.style.display = 'none'
-            loginView.style.display = 'none'
-            homeView.style.display = 'block'
-            homeView.innerText = "Hello, " + name
-            break
-        }
+            loginView.style.display = 'block'
+            homeView.style.display = 'none'
     }
 
 }
@@ -130,32 +135,39 @@ loginRegisterLink.onclick = function (event){
          var email = emailInput.value
          var password = passwordInput.value
 
+         console.log(email)
+         console.log(password)
+
          var user = {}
+         var findUser = false
 
         
          user.email = email
          user.password = password
 
-         //var found = false;
+         
         for(var i = 0; i < users.length; i++){
-            if (users[i].email == user.email){
-            //found = true;
-            console.log(`Welcome ${users[i].name}`)
-            registerView.style.display = 'none'
-            loginView.style.display = 'none'
-            homeView.style.display = 'block'
-            homeView.innerText = "Hello, " + users[i].name
-            break
 
-         } else {
-            //found = false;
-            console.log(`ERROR: Unregistered user`)
-            emailInput.value = ''
-            passwordInput.value = ''
-            registerView.style.display = 'none'
-            homeView.style.display = 'none'
-            break
-        }
+            if (users[i].email == user.email && users[i].password == user.password){
+                findUser = true
+                nameLoginUser = users[i].name
+                console.log(findUser);
+            }
+            
+    }
+
+    if (findUser == true){
+        alert(`Welcome ${nameLoginUser}`);
+        registerView.style.display = 'none'
+        loginView.style.display = 'none'
+        homeView.style.display = 'block'
+        homeView.innerText = "Hello, " + nameLoginUser
+    } else {
+        alert(`ERROR: Unregistered user or invalid password`);
+        emailInput.value = ''
+        passwordInput.value = ''
+        registerView.style.display = 'none'
+        homeView.style.display = 'none'
     }
 
 }
