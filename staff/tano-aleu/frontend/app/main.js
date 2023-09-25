782669ef81b55bd9609e67f787ed6d84a58dc524
Tano Aleu-Mosteiro <tanoalmost@gmail.com>
2023-09-25 19:13:29 +0200

    update login/register

--- staff/tano-aleu/frontend/app/main.js
+++ staff/tano-aleu/frontend/app/main.js
@@ -1,92 +1,110 @@
-//storage
+// Obtención de elementos del DOM y ocultamiento inicial:
 
-var users = []
-
-function registerUser(name, email, password) {
-    var user = {
-        name: name,
-        email: email,
-        password: password
-    }
-
-}
-
-//register
-
+var loginView = document.getElementById('login')
+var homeView = document.getElementById('home')
 var registerView = document.getElementById('register')
 
 registerView.style.display = 'none'
+homeView.style.display = 'none'
 
-var registerLoginLink = registerView.querySelector('a')
+// Obtencion de enlaces de cambio de vista
 
-registerLoginLink.onclick = function (event) {
+var loginLink = registerView.querySelector('a')
+var registerLink = loginView.querySelector('a')
+
+// Array para almacenar los usuarios
+
+var users = []
+
+// Manejo de eventos para cambiar entre vistas
+
+loginLink.onclick = function (event) {
     event.preventDefault()
-
     registerView.style.display = 'none'
     loginView.style.display = 'block'
 }
+
+registerLink.onclick = function (event) {
+    event.preventDefault()
+    registerView.style.display = 'block'
+    loginView.style.display = 'none'
+
+}
+
+// Manejo del formulario de registro
 
 var registerForm = registerView.querySelector('form')
 
 registerForm.onsubmit = function (event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    var nameInput = registerForm.querySelector('#name')
-    var emailInput = registerForm.querySelector('#email')
-    var passwordInput = registerForm.querySelector('#password')
+    var nameInput = registerForm.querySelector('#name');
+    var emailInput = registerForm.querySelector('#registerEmail');
+    var passwordInput = registerForm.querySelector('#registerPassword');
 
-    var name = nameInput.value
+    var name = nameInput.value;
+    var email = emailInput.value;
+    var password = passwordInput.value;
+
+    if (name === '' || email === '' || password === '') {
+        alert('Please enter your name, email, and password.');
+    } else {
+        var userExists = users.some(function (user) {
+            return user.email === email;
+        });
+
+        if (userExists) {
+            alert('The user already exists. Please log in instead of registering.');
+        } else {
+            var user = {
+                name: name,
+                email: email,
+                password: password
+            };
+
+            users.push(user);
+
+            nameInput.value = '';
+            emailInput.value = '';
+            passwordInput.value = '';
+
+            registerView.style.display = 'none';
+            loginView.style.display = 'block';
+        }
+    }
+};
+
+
+// Manejo del formulario de inicio de sesion
+
+var loginForm = loginView.querySelector('form')
+
+loginForm.onsubmit = function (event) {
+    event.preventDefault();
+
+    var emailInput = loginForm.querySelector('#loginEmail')
+    var passwordInput = loginForm.querySelector('#loginPassword')
+
     var email = emailInput.value
     var password = passwordInput.value
 
-    var user = {}
+    var user = users.find(function (user) {
+        return user.email === email && user.password === password
+    })
 
-    user.name = name
-    user.email = email
-    user.password = password
+    if (user) {
 
+        var username = user.name
 
-    // TODO check user is a new, otherwise show error
+        loginView.style.display = 'none'
+        homeView.style.display = 'block'
+        document.getElementById('username').textContent = `Hello, ${username}!`
 
-    users.push(user)
+    } else {
 
-    nameInput.value = ''
-    emailInput.value = ''
-    passwordInput.value = ''
-
-    registerView.style.display = 'none'
-    loginView.style.display = 'block'
-
-    console.log(user)
-
+        alert('The E-mail or password is incorrect. Please try again')
+    }
 }
-
-//login
-
-var loginView = document.getElementById('login')
-var loginRegisterLink = loginView.querySelector('a')
-
-loginRegisterLink.onclick = function (event) {
-    event.preventDefault()
-
-    loginView.style.display = 'none'
-    registerView.style.display = 'block'
-
-    //TODO implement login functionality
-
-}
-
-
-//home
-
-var homeView = document.getElementById('home')
-
-homeView.style.display = 'none'
-
-console.log('Lista de usuarios:', users);
-
-
-
 
 
 
