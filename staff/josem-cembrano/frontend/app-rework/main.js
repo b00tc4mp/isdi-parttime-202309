e05ead1fbf00c97a5167978de19095ea61ba8030
josem-cembrano <isdichema@gmail.com>
2023-09-27 10:39:11 +0200

    changes and create files

--- staff/josem-cembrano/frontend/app-rework/main.js
+++ staff/josem-cembrano/frontend/app-rework/main.js
@@ -1,47 +1,100 @@
-var users = [
+var users = [];
+var user = {};
 
-    {
-        name: 'qwe',
-        email: 'qwe@gmail.com',
-        password: 'qwe',
-    }
+var loginView = document.getElementById('login');
+var registerView = document.getElementById('register');
+var homeView = document.getElementById('home');
+registerView.style.display = 'none';
+homeView.style.display = 'none';
 
-]
-
-var registerView = document.getElementById('register')
-registerView.style.display = 'none'
-
-var registerLoginLink = registerView.querySelector('a')
+var registerLoginLink = document.getElementById('registerLoginLink');
+var loginRegisterLink = document.getElementById('loginRegisterLink');
 
 registerLoginLink.onclick = function (event) {
-    event.preventDefautl()
-    
-    registerView.style.display = 'none'
-    loginView.style.display = 'block'
-}
+    event.preventDefault();
 
+    registerView.style.display = 'none';
+    loginView.style.display = 'block';
+};
 
-var registerForm = registerView.querySelector('form') //(querySelector)=> buscar el elemento form.
+loginRegisterLink.onclick = function (event) {
+    event.preventDefault();
 
+    loginView.style.display = 'none';
+    registerView.style.display = 'block';
+};
+
+var registerForm = document.getElementById('registerForm');
 registerForm.onsubmit = function (event) {
-    event.preventDefautl()
+    event.preventDefault();
 
-    var emailImput = registerForm.querySelector('#email')
-    var email = emailImput.ariaValueMax
+    var nameInput = registerForm.querySelector('#name');
+    var emailInput = registerForm.querySelector('#email');
+    var passwordInput = registerForm.querySelector('#password');
 
-    var userFound = false
+    var name = nameInput.value;
+    var email = emailInput.value;
+    var password = passwordInput.value;
 
-    for (var i = 0; i < users.length && !userFound; i++) {
-        var user = users[i]
+    // Verificar si el usuario ya existe
+    var userExists = users.some(function (user) {
+        return user.email === email;
+    });
 
-        if (user.email === email)
-            userfound = true
+    if (userExists) {
+        // Mostrar un mensaje de error.
+        alert('Este usuario ya ha sido registrado');
+        nameInput.value = '';
+        emailInput.value = '';
+        passwordInput.value = '';
+    } else {
+        // Si el usuario no existe, agregarlo a la lista de usuarios
+        user = {
+            name: name,
+            email: email,
+            password: password
+        };
+
+        // TODO: Verificar y agregar el usuario a tu base de datos (array)
+        users.push(user);
+
+        nameInput.value = '';
+        emailInput.value = '';
+        passwordInput.value = '';
+
+        registerView.style.display = 'none';
+        loginView.style.display = 'block';
     }
+};
 
-    if (userFound)
-        alert('Este usuario ya existe')
+function loginUser(email, password) {
+    // Verificar si el usuario existe en la base de datos
+    var user = users.find(function (user) {
+        return user.email === email && user.password === password;
+    });
 
-        return
+    if (user) {
+        // Iniciar sesión exitosa, redirigir al usuario a la página de bienvenida
+        loginView.style.display = 'none';
+        homeView.style.display = 'block';
+    } else {
+        // Mostrar un mensaje de error si las credenciales son incorrectas
+        alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+    }
 }
+
+var loginForm = document.getElementById('loginForm');
+loginForm.onsubmit = function (event) {
+    event.preventDefault();
+
+    var emailInput = loginForm.querySelector('#loginEmail');
+    var passwordInput = loginForm.querySelector('#loginPassword');
+
+    var email = emailInput.value;
+    var password = passwordInput.value;
+
+    // Llamar a la función de inicio de sesión
+    loginUser(email, password);
+};
 
 
