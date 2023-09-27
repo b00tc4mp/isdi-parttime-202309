a649e3b828a8b24aaaaa15c30f62e9e2d21c10d3
eneko9555 <eneko_55@hotmail.com>
2023-09-27 19:52:11 +0200

    fix register function

--- staff/eneko-fernandez/frontend/app/data.js
+++ staff/eneko-fernandez/frontend/app/data.js
@@ -1,3 +1,8 @@
 var users = [
     {name :"eneko", email: "correo@correo.com", password: "123"}
 ]
+
+function registerUser(name, email, password) {
+  var user = { name, email, password }
+  Object.values(user).some((val) => val !== '' && users.push(user))
+}
--- staff/eneko-fernandez/frontend/app/logic.js
+++ staff/eneko-fernandez/frontend/app/logic.js
@@ -1,7 +1,3 @@
-function registerUser(name, email, password) {
-  var user = { name, email, password }
-  Object.values(user).some((val) => val !== '' && users.push(user))
-}
 
 function existUser(emailValue) {
   return users.some((user) => user.email === emailValue)
