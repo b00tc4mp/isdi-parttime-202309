e0ff27e90698143a2cb31884b1c5b119f27162f4
Isabel Domínguez Sánchez <134840727+IsaDScmd@users.noreply.github.com>
2023-09-30 12:54:53 +0200

    add old version

---
+++ staff/isabel-dominguez/frontend/app.1/data.js
@@ -0,0 +1,22 @@
+var users = []
+
+function createUser(name, email, password) {
+    var user = {}
+
+    user.name = name
+    user.email = email
+    user.password = password
+
+    users.push(user)
+}
+
+function findUserByEmail(email) {
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i]
+
+        if (user.email === email)
+            return user
+    }
+
+    return null
+}
