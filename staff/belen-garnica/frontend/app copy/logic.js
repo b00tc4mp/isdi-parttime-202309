0779b9064c6d0016e04c503688f33b1d4325c3db
Belen Garnica <belengarnicaf@gmail.com>
2023-10-01 14:38:19 +0200

    Paneles de cambio

---
+++ staff/belen-garnica/frontend/app copy/logic.js
@@ -0,0 +1,48 @@
+function registerUser(name, email, password) {
+    validateText(name, 'name')
+    validateText(email, 'email')
+    validateText(password, 'password')
+
+    var user = findUserByEmail(email)
+
+    if (user)
+        throw new Error('user already exists')
+
+    createUser(name, email, password)
+}
+
+function authenticateUser(email, password) {
+    validateText(email, 'email')
+    validateText(password, 'password')
+
+    var user = findUserByEmail(email)
+
+    if (!user || user.password !== password)
+        throw new Error('wrong credentials')
+}
+
+function retrieveUser(email) {
+    validateText(email, 'email')
+    
+    var user = findUserByEmail(email)
+
+    if (!user)
+        throw new Error('user not found')
+
+    return user
+}
+
+function changeEmail(email, newEmail) {
+    validateText(email, 'email')
+    validateText(newEmail, 'email')
+
+    var user = findUserByEmail(email)
+
+    if (!user) {
+        throw new Error('Email not found')
+    }
+
+    user.email = newEmail
+
+    return user
+}
