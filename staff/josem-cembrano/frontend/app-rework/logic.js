45df6b1ede3f60aa3b6ea7900b7ca8f6acc3af93
josem-cembrano <isdichema@gmail.com>
2023-10-04 10:21:50 +0200

    update files app

--- staff/josem-cembrano/frontend/app-rework/logic.js
+++ staff/josem-cembrano/frontend/app-rework/logic.js
@@ -31,3 +31,37 @@ function retrieveUser(email) {
 
     return user
 }
+
+function changeUserEmail(email, newEmail, newEmailConfirm, password) {
+    validateText(email, 'email')
+    validateText(newEmail, 'new email')
+    validateText(newEmailConfirm, 'new email confirm')
+    validateText(password, 'password')
+
+    var user = findUserByEmail(email)
+
+    if (!user || user.password !== password)
+        throw new Error('wrong credentials')
+
+    if (newEmail !== newEmailConfirm)
+        throw new Error('new email and its confirmation do not match')
+
+    user.email = newEmail
+}
+
+function changeUserPassword(email, newPassword, newPasswordConfirm, password) {
+    validateText(email, 'email')
+    validateText(newPassword, 'new password')
+    validateText(newPasswordConfirm, 'new password confirm')
+    validateText(password, 'password')
+
+    var user = findUserByEmail(email)
+
+    if (!user || user.password !== password)
+        throw new Error('wrong credentials')
+
+    if (newPassword !== newPasswordConfirm)
+        throw new Error('new password and its confirmation do not match')
+
+    user.password = newPassword
+}
