8dc03fed7edb065762e5171f1231f2715d04fd21
Abel Prieto Martín <abelpriem94@hotmail.com>
2023-10-05 22:08:39 +0200

    add arrays field and modify strings field too

---
+++ staff/abel-prieto/FRONTEND/STRINGS/lastIndexOf.spec.js
@@ -0,0 +1,14 @@
+console.log('TEST lastIndexOf')
+
+console.log('CASE for string "hello world" lastIndexOf "o" results in 7')
+console.log(lastIndexOf('hello world', 'o'))
+// 7
+
+console.log('CASE for string "hello world" lastIndexOf "1" results in 9')
+console.log(lastIndexOf('hello world', '1'))
+// 9
+
+console.log('CASE for string "hello world" lastIndexOf "a" results in -1')
+console.log(lastIndexOf('hello world', 'a'))
+// -1
+
