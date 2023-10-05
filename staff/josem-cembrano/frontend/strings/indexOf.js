18c5d49559f61dbd793947764b783c7aecdcb840
josem-cembrano <isdichema@gmail.com>
2023-10-05 11:23:24 +0200

    challenge indexOf

---
+++ staff/josem-cembrano/frontend/strings/indexOf.js
@@ -0,0 +1,10 @@
+function indexOf(string, searchString) {
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === searchString) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(indexOf('hola mundo', 'm'));
