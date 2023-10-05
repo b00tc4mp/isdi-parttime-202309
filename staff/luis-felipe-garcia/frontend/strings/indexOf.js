354b08f628bd8b48fe2bc635c3e36313e13608bd
Luis Felipe García <gpintolf@gmail.com>
2023-10-05 21:43:05 +0200

    add strings and arrays standalone functions

--- staff/luis-felipe-garcia/frontend/strings/indexOf.js
+++ staff/luis-felipe-garcia/frontend/strings/indexOf.js
@@ -1,8 +1,8 @@
-function indexOf (string, chart) {
-    for (var i=0; i< string.length; i++)
-    if (string[i] === chart) {
-        return i
-    }
+function indexOf(string, chart) {
+    for (var i = 0; i < string.length; i++)
+        if (string[i] === chart) {
+            return i
+        }
     return '-1'
 
 }
--- staff/luis-felipe-garcia/frontend/strings/lastIndexOf.js
+++ staff/luis-felipe-garcia/frontend/strings/lastIndexOf.js
@@ -1,5 +1,5 @@
 function lastIndexOf (string, chart) {
-    for (var i=string.length; i>=0 ; i--)
+    for (var i=string.length-1; i>=0 ; i--)
     if (string[i] === chart) {
         return i
     }
