c4081c6022425ccac0c3dbe6f5dba84ec4f64f31
Anna Cuenca <acuencabo@gmail.com>
2023-10-05 17:02:23 +0200

    update app status

---
+++ staff/anna-cuenca/frontend/strings/indexOf.js
@@ -0,0 +1,10 @@
+function indexOf(string, searchString) {
+    for (var i = 0; i < string.length; i++){
+        if (string[i] === searchString){
+            return i
+        }  
+    }
+
+    return -1
+}
+
