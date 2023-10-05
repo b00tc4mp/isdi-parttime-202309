0db241e21da20e3a209c3151591e4f1fe3bc8e60
josem-cembrano <isdichema@gmail.com>
2023-10-05 11:37:13 +0200

    change modify 'to do' indexOf

--- staff/josem-cembrano/frontend/strings/indexOf.js
+++ staff/josem-cembrano/frontend/strings/indexOf.js
@@ -1,10 +1,14 @@
 function indexOf(string, searchString) {
     for (let i = 0; i < string.length; i++) {
         if (string[i] === searchString) {
-            return i;
+            return i
+        }
+        else {
+            return -1
         }
     }
-    return -1;
 }
 
 console.log(indexOf('hola mundo', 'm'));
+/* utilizo el blucle for porq es necesario recorrer el string al mismo tiempo que compara el elemento que quiero buscar con los que hay en el string, 
+ya que el indexOf busca recorriendo el string,*/
