decb920cccf8c7e859f84f46cc72d7136fd56551
Ana del Río Collar <anadelriocollar@ana.local>
2023-10-06 21:32:01 +0200

    add solutions indexOf and lastIndexOf algorithm

--- staff/ana-delrio/frontend/strings/indexOf.js
+++ staff/ana-delrio/frontend/strings/indexOf.js
@@ -11,3 +11,4 @@ function indexOf(string, searchString) {
 // El método indexOf en JavaScript se utiliza para buscar la primera aparición de un elemento específico en un array o una cadena de texto 
 // y devuelve la posición (índice) de esa primera aparición. 
 // Si el elemento no se encuentra en el array o la cadena, devuelve -1.
+
