c4b930c27f2ad76e94c5fc1392749aa135ce2476
Ana del Río Collar <anadelriocollar@ana.local>
2023-10-07 12:39:42 +0200

    add strings and arrays standalone functions

---
+++ staff/ana-delrio/frontend/strings/includes.spec.3.js
@@ -0,0 +1,48 @@
+console.log('TEST includes')
+
+console.log('CASE for string "hola mundo" includes "m" results in true')
+console.log(includes('hola mundo', 'm'))
+//true
+
+console.log('CASE for string "hola mundo" includes "w" results in false')
+console.log(includes('hola mundo', 'w'))
+//false
+
+console.log('CASE for string "hola mundo" includes "h" results in true')
+console.log(includes('hola mundo', 'h'))
+//true
+
+console.log('CASE for string "hola mundo" includes "o" results in true')
+console.log(includes('hola mundo', 'o'))
+//true
+
+console.log('CASE for string "hola mundo" includes "o" results n false')
+console.log(includes('hola mundo', 'H'))
+//false
+
+console.log('CASE for string "hola mundo" includes "mu" results in false')
+console.log(includes('hola mundo', 'mu'))
+//false
+//se me rompe :(
+
+console.log('CASE for string "hola mundo" includes "mo" results in false')
+console.log(includes('hola mundo', 'mo'))
+//false
+
+console.log('CASE for string "hola mundo" includes "la" results in true')
+console.log(includes('hola mundo', 'la'))
+//true
+
+console.log('CASE for string "hola mundo" includes "las" results in false')
+console.log(includes('hola mundo', 'las'))
+//false
+//ya no funciona :(
+
+console.log('CASE for string "hola mundo" includes "la " results in true')
+console.log(includes('hola mundo', 'la '))
+//true
+
+console.log('CASE for string "hola mundo" includes "wo" results in false')
+console.log(includes('hola mundo', 'wo'))
+//false
+// me sale undefined :(
