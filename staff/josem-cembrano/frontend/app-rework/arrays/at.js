f80596752e15134ef8b55b5e2803041216f75867
josem-cembrano <isdichema@gmail.com>
2023-10-08 19:43:41 +0200

    edit challenge finish

--- staff/josem-cembrano/frontend/app-rework/arrays/at.js
+++ staff/josem-cembrano/frontend/app-rework/arrays/at.js
@@ -1,3 +1,4 @@
+var array = ['hola', 'mundo']
 var at = function (array, searchPosition) {
     if (searchPosition >= 0) return array[searchPosition]
     else { return array[array.length + searchPosition] }
--- staff/josem-cembrano/frontend/app-rework/arrays/at.spec.js
+++ staff/josem-cembrano/frontend/app-rework/arrays/at.spec.js
@@ -1,13 +1,13 @@
 console.log('TEST at')
 
-console.log('CASE for arrays "hola mundo" at -2 should result in "d"')
-console.log(at('hola mundo', -2))
-// "d"
+console.log('CASE for arrays "hola mundo" at -1 should result in "mundo"')
+console.log(at(array, -1))
+// "mundo"
 
-console.log('CASE for arrays "hola mundo" at 4 should result in " "')
-console.log('>', at('hola mundo', 4), '<')
-// " "
+console.log('CASE for arrays "hola mundo" at 0 should result in "hola"')
+console.log('>', at(array, 0), '<')
+// "hola"
 
-console.log('CASE for arrays "hola mundo" at 10 should result in ""')
-console.log('>', at('hola mundo', 10), '<')
-// ""
+console.log('CASE for arrays "hola mundo" at -3 should result in "undefined"')
+console.log('>', at(array, -3), '<')
+// "undefined"
