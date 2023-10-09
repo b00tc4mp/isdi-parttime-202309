d5a067e2ac29fc9deeca1f660cfaf096bb41a022
Cristian Rubert <75452065+cristianrubert@users.noreply.github.com>
2023-10-09 16:56:34 +0000

    exercise array at and concat

--- staff/cristian-rubert/frontend/app/arrays/concat.spec.js
+++ staff/cristian-rubert/frontend/app/arrays/concat.spec.js
@@ -1,18 +1,18 @@
-// console.log('TEST concat')
+console.log('TEST concat')
 
+console.log('CASE for array [5, 12, 8, 130, 44], [10, 20, 30, 40, 50] concat "3" results is [5, 12, 8, 130, 44, 10, 20, 30, 40, 50]')
+console.log(concat([5, 12, 8, 130, 44], [10, 20, 30, 40, 50]))
+// 130
 
-// console.log('CASE for array ([5, 12, 8, 130, 44] concat "3" results is "130"')
-// console.log(concat([5, 12, 8, 130, 44], 4))
-// // 130
+console.log('CASE for array [5, 12, 8, 130, 44], [10, 20, 30, 40, 50], [a, b, c, d] concat 0 results is [5, 12, 8, 130, 44, 10, 20, 30, 40, 50, a, b, c, d] ')
+console.log(concat([5, 12, 8, 130, 44], [10, 20, 30, 40, 50], ['a', 'b', 'c', 'd']))
+// 5
 
-// console.log('CASE for array ([5, 12, 8, 130, 44] concat 0 results is 5')
-// console.log(concat([5, 12, 8, 130, 44], 4))
-// // 5
+console.log('CASE for array [5, 12, 8, 130, 44], [20, 21, 22, 23, 24, 25], [Manu, Ana, Judy, Isabel, Luis, Belen ] concat  results is [5, 12, 8, 130, 44, 20, 21, 22, 23, 24, 25, Manu, Ana, Judy, Isabel, Luis, Belen ]')
+console.log(concat([5, 12, 8, 130, 44], [20, 21, 22, 23, 24, 25], ['Manu', 'Ana', 'Judy', 'Isabel', 'Luis', 'Belen' ]))
+// 44
 
-// console.log('CASE for array ([5, 12, 8, 130, 44] concat 4 results is 44')
-// console.log(concat([5, 12, 8, 130, 44], 4))
-// // 44
+console.log('CASE for array [5, 12, 8, 130, 44], [100, 200, 300, 400, 500], [Abel, Edgar, Benja, eneko, chema, tano, miguel ] concat 1 results is [5, 12, 8, 130, 44], [100, 200, 300, 400, 500], [Abel, Edgar, Benja, eneko, chema, tano, miguel ] ')
+console.log(concat([5, 12, 8, 130, 44], [100, 200, 300, 400, 500], ['Abel', 'Edgar', 'Benja', 'eneko', 'chema', 'tano', 'miguel' ]))
+// 12
 
-// console.log('CASE for array ([5, 12, 8, 130, 44] concat 1 results is 12')
-// console.log(concat([5, 12, 8, 130, 44], 1))
-// // 12
