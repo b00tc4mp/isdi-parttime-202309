f73239db8da2a643738a9bf459378887473ba057
Isabel Domínguez Sánchez <134840727+IsaDScmd@users.noreply.github.com>
2023-10-10 20:58:42 +0200

    add functions forEach, map and filter

---
+++ staff/isabel-dominguez/frontend/arrays/map.spec.js
@@ -0,0 +1,13 @@
+console.log('TEST map')
+
+var a = [1, 4, 9, 16]
+
+console.log('CASE map array [1, 4, 9, 16] to new array with elements multiplied by 2 in the console')
+
+var mappedArray = map(a, function (x) { return x * 2 })
+console.log(mappedArray);
+
+console.log('CASE map array [1, 4, 9, 16] to new array with elements multiplied by 5 in the console')
+
+var mappedArray = map(a, function (x) { return x * 5 })
+console.log(mappedArray);
