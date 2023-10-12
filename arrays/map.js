53e91a995f0c1557c1c1096e665aaf38f57e239a
cristianrubert1 <alurub8913@ieselcaminas.org>
2023-10-12 12:55:04 +0200

    files errors 2

--- arrays/map.js
+++
@@ -1,9 +0,0 @@
-function map(array, callback) {
-    var result = []
-    for (var i = 0; i < array.length; i++) {
-        var element = array[i]
-        result[i] = callback(element)
-    }
-    return result
-
-}
