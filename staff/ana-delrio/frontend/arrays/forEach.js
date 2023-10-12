b88ac239a5d49941b4f63fdb000675c284d25269
Ana del Río Collar <anadelriocollar@ana.local>
2023-10-12 20:21:18 +0200

    add arrays forEach

---
+++ staff/ana-delrio/frontend/arrays/forEach.js
@@ -0,0 +1,13 @@
+
+// DEFINICION de la función personalizada 'forEach' que toma un array y una función de callback
+function forEach(array, callback) {
+    // Usamos un bucle 'for' para recorrer el array 'array'
+    for (var i = 0; i < array.length; i++) {
+        // Obtenemos el valor del elemento en la posición 'i' y lo almacenamos en la variable 'v'
+        var v = array[i]
+
+        // Llamamos a la función de callback pasando 'v' como argumento
+        callback(v)
+    }
+}
+
---
+++ staff/ana-delrio/frontend/arrays/forEach.spec.js
@@ -0,0 +1,22 @@
+console.log('TEST forEach')
+
+console.log('CASE for each element in [10, 20, 30] print it in the console')
+
+
+// DECLARACION de un array llamado 'a' con valores [10, 20, 30]
+var a = [10, 20, 30]
+
+// Llamada a la función 'forEach' con el array 'a' y una función de callback anónima
+forEach(a, function (v) {
+    // En cada iteración, esta función de callback imprime el valor 'v' en la consola
+    console.log(v)
+})
+
+
+console.log('CASE for each element in [10, 20, 30] print it multiplied by 10 in the console')
+
+var a = [10, 20, 30]
+
+forEach(a, function (v) {
+    console.log(v * 10)
+})
