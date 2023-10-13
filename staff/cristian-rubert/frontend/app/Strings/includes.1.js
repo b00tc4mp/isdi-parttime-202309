285bb9ccccf3108eac5c164c774bafd1b0a5b9f2
cristianrubert1 <alurub8913@ieselcaminas.org>
2023-10-13 18:17:05 +0200

    copy all files

--- staff/cristian-rubert/frontend/app/Strings/includes.1.js
+++
@@ -1,8 +0,0 @@
-function includes(string, ascertai) {
-    for(let i = 0; i < string.length; i++) {
-        if (string[i] === ascertai) {
-            return true
-        }
-    }
-    return false
-}
