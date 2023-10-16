ded20d72435ad0fa467673eb3bc295b1f31d762d
Isabel Domínguez Sánchez <134840727+IsaDScmd@users.noreply.github.com>
2023-10-16 18:48:49 +0200

    modify concat function and add case test

--- staff/isabel-dominguez/frontend/curries/Curry.prototype.concat.js
+++ staff/isabel-dominguez/frontend/curries/Curry.prototype.concat.js
@@ -5,19 +5,13 @@ Curry.prototype.concat = function () {
         concatenatedArray[concatenatedArray.length] = this[i]
     }
 
-    for (var i = 0; i < arguments.length; i++) {
-        var currentArray = arguments[i]
-        for (var j = 0; j < currentArray.length; j++) {
-            concatenatedArray[concatenatedArray.length] = currentArray[j]
+    for (var i = 0; i < arguments.length; i++) {  //Este itera sobre los arrays pasados como argumentos
+        var otherArrays = arguments[i]
+        for (var j = 0; j < otherArrays.length; j++) {  // Este itera sobre los elementos de los arrays
+            concatenatedArray[concatenatedArray.length] = otherArrays[j]
         }
     }
 
-    for (var i = 0; i < concatenatedArray.length; i++) {
-        this[i] = concatenatedArray[i]
-    }
-
-    this.length = concatenatedArray.length
-
-    return this
+    return concatenatedArray
 }
 
