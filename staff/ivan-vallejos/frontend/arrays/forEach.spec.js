18bba6cdf9204a6b018d4e043bfb320bc40e98b8
Jorge Ivan Vallejos Cardozo <navivallejos@gmail.com>
2023-10-17 17:40:14 +0200

    Add folder arrays and curries

---
+++ staff/ivan-vallejos/frontend/arrays/forEach.spec.js
@@ -0,0 +1,17 @@
+console.log('Test forEach')
+
+console.log('Case forEach element in [10,20,30] print it in the console')
+
+var a = [10,20,30]
+
+forEach(a, function(v){
+    console.log(v)
+})
+
+console.log('Case forEach element in [10,20,30] pirnt it multiplied by 10 in the console')
+
+var a = [10,20,30]
+
+forEach(a, function(v){
+    console.log(v*10)
+})
