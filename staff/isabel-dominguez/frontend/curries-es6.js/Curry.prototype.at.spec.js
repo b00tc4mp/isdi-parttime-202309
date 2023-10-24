7041d363fa53eabd02d88289074d32dc5459fef0
Isabel Domínguez Sánchez <134840727+IsaDScmd@users.noreply.github.com>
2023-10-24 18:51:11 +0200

    work in at function

--- staff/isabel-dominguez/frontend/curries-es6.js/Curry.prototype.at.spec.js
+++ staff/isabel-dominguez/frontend/curries-es6.js/Curry.prototype.at.spec.js
@@ -3,40 +3,40 @@ console.log("*******************************************************************
 
 CASE('returns the item at that index "0" results in "10"')
 
-let c = new Curry(10, 20, 30)
-let index = c.at(0)
+let curryObject1 = new Curry(10, 20, 30)
+let index1 = curryObject1.at(0)
 
-console.log(index)
+console.log(index1)
 // 10
-console.log(c)
+console.log(curryObject1)
 // Curry { 0: 10, 1: 20, 2: 30, length: 3 }
 
 CASE('returns the item at that index "2" results in "30"')
 
-let c = new Curry(10, 20, 30)
-let index = c.at(2)
+let curryObject22 = new Curry(10, 20, 30)
+let index2 = curryObject22.at(2)
 
-console.log(index)
+console.log(index2)
 // 30
-console.log(c)
+console.log(curryObject22)
 // Curry { 0: 10, 1: 20, 2: 30, length: 3 }
 
 CASE('returns the item at that index "-3" results in "10"')
 
-let c = new Curry(10, 20, 30)
-let index = c.at(-3)
+let curryObject33 = new Curry(10, 20, 30)
+let index3 = curryObject33.at(-3)
 
-console.log(index)
+console.log(index3)
 // 10
-console.log(c)
+console.log(curryObject33)
 // Curry { 0: 10, 1: 20, 2: 30, length: 3 }
 
 CASE('returns the item at that index "-1" results in "30"')
 
-let c = new Curry(10, 20, 30)
-let index = c.at(-1)
+let curryObject44 = new Curry(10, 20, 30)
+let index4 = curryObject44.at(-1)
 
-console.log(index)
+console.log(index4)
 // 30
-console.log(c)
+console.log(curryObject44)
 // Curry { 0: 10, 1: 20, 2: 30, length: 3 }
