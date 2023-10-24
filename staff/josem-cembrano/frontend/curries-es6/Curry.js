568da7a0a942747e8d1b7ea81dcd12fc5ec1d939
josem-cembrano <isdichema@gmail.com>
2023-10-24 12:16:30 +0200

    transcription

--- staff/josem-cembrano/frontend/curries-es6/Curry.js
+++ staff/josem-cembrano/frontend/curries-es6/Curry.js
@@ -8,8 +8,8 @@ class Curry {
             } else if (Number.isInteger(args[0]) && args[0] < 0 || typeof args[0] === 'number' && !Number.isInteger(args[0]))
                 throw new RangeError('Invalid curry length')
 
-        for (var i = 0; i < args.length; i++) {
-            var argument = args[i]
+        for (let i = 0; i < args.length; i++) {
+            const argument = args[i]
 
             this[i] = argument
         }
