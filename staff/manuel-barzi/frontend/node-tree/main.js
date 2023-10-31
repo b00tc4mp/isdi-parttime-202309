e406518c8d646b997c5b7cabbc30d10e3601f828
Manuel Barzi <manuelbarzi@gmail.com>
2023-10-31 21:27:22 +0100

    add node-tree and proto-chain challenges; move first steps forward to react (for app)

---
+++ staff/manuel-barzi/frontend/node-tree/main.js
@@ -0,0 +1,11 @@
+function nodeTree(element, level = 0) {
+    const children = element.childNodes
+
+    children.forEach(child => {
+        console.log(`${level === 0 ? '' : '├─'} ${'\t'.repeat(level)} ${child.constructor.name} - ${child.tagName}`)
+
+        nodeTree(child, level + 1)
+    })
+}
+
+nodeTree(document)
