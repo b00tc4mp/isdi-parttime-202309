55564a9935363ca3e611daa5352f67a6847fd2de
Abel Prieto Martín <abelpriem94@hotmail.com>
2023-11-10 21:51:22 +0100

    set 'list-post-favs' and logic

---
+++ staff/abel-prieto/FRONTEND/APP/REACT/APP.9/App.jsx
@@ -0,0 +1,31 @@
+// APP
+
+function App() {
+    const viewState = React.useState('login')
+    // Es un Array. Devuelve el 'ESTADO' del componente, en este caso, de la vista
+    // La función useState se compone de view | setView. 
+
+    const view = viewState[0]
+    const setView = viewState[1]
+
+    function handleRegisterShow() {
+        setView('register')
+        // Cambiamos la vista a 'REGISTER'
+    }
+
+    function handleLoginShow() {
+        setView('login')
+        // Cambiamos la vista a 'LOGIN'
+    }
+
+    function handleHomeShow() {
+        setView('home')
+        // Cambiamos la vista a 'HOME'
+    }
+
+    return <>
+        {view === 'login' && <Login onRegisterClick={handleRegisterShow} onSuccess={handleHomeShow}  />}
+        {view === 'register' && <Register onLoginClick={handleLoginShow} onSuccess={handleLoginShow} />}
+        {view === 'home' && <Home onLogoutClick={handleLoginShow} />}
+    </>
+}
---
+++ staff/abel-prieto/FRONTEND/APP/REACT/APP.9/main.jsx
@@ -0,0 +1,15 @@
+// LOGIC
+const logic = new Logic
+
+// REACT 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<App tittle="APP React v0.9" />)
+
+
+// - - - - - - - - - - - - NOTAS - - - - - - - - - - - -
+
+// En los elementos <input> se necesita tener un cierre < />
+// El componente Login ha de ser llamado por root.render mediante la etiqueta <Login /> SIEMPRE!
+// Los class pasan a llamarse 'className' y for, 'htmlFor'
+// Las vistas se ensamblan y desensamblan
