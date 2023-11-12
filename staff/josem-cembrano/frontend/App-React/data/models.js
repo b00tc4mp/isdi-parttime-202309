94d7b9fc69f157fe3c0099e20e7532a84770e5ab
josem-cembrano <isdichema@gmail.com>
2023-11-12 19:04:37 +0100

    update app-react/subtitution managers => collection

--- staff/josem-cembrano/frontend/App-React/data/models.js
+++ staff/josem-cembrano/frontend/App-React/data/models.js
@@ -1,9 +1,10 @@
 class User {
-    constructor(id, name, email, password) {
+    constructor(id, name, email, password, favs) {
         this.id = id
         this.name = name
         this.email = email
         this.password = password
+        this.favs = favs
     }
 }
 
@@ -14,5 +15,15 @@ class Post {
         this.image = image
         this.text = text
         this.likes = likes
+    }
+}
+
+class CreditCard {
+    constructor(id, user, fullName, number, expirationDate) {
+        this.id = id
+        this.user = user
+        this.fullName = fullName
+        this.number = number
+        this.expirationDate = expirationDate
     }
 }
