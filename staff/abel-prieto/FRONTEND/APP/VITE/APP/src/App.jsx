cd30e3835159898f69d3a82a7b5e3868a3aae7b8
Abel Prieto Martín <abelpriem94@hotmail.com>
2023-11-22 18:05:06 +0100

    update APP with vite with all views & functions

--- staff/abel-prieto/FRONTEND/APP/VITE/APP/src/App.jsx
+++ staff/abel-prieto/FRONTEND/APP/VITE/APP/src/App.jsx
@@ -1,11 +1,35 @@
+import React from "react"
+import Login from "./views/Login"
+import Register from "./views/Register"
+import Home from "./views/Home"
+import { useState } from "react"  // Import method useState 
 
+// APP
 
 function App() {
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  )
+
+  const [view, setView] = useState('login')
+
+  function handleRegisterShow() {
+    setView('register')
+    // Cambiamos la vista a 'REGISTER'
+  }
+
+  function handleLoginShow() {
+    setView('login')
+    // Cambiamos la vista a 'LOGIN'
+  }
+
+  function handleHomeShow() {
+    setView('home')
+    // Cambiamos la vista a 'HOME'
+  }
+
+  return <>
+    {view === 'login' && <Login onRegisterClick={handleRegisterShow} onSuccess={handleHomeShow} />}
+    {view === 'register' && <Register onLoginClick={handleLoginShow} onSuccess={handleLoginShow} />}
+    {view === 'home' && <Home onLogoutClick={handleLoginShow} />}
+  </>
 }
 
 export default App
