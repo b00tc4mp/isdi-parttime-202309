ed5aab5a549a62a0759106aee1c0a3cf7ea042c0
Anna Cuenca <acuencabo@gmail.com>
2023-11-22 15:47:25 +0100

    add last version of the app before vite

---
+++ staff/anna-cuenca/frontend/app.19/main.jsx
@@ -0,0 +1,19 @@
+// logic
+
+const logic = new Logic
+
+// react
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
+
+// Parser es como un interprete
+
+// const parser = new DOMParser (ejemplo)
+// const doc = àrser.parseFromString(xhr.response, 'text/html')
+
+// para conseguir trabajar con la app como si fuese un servidodr, hay que hacer asincrona la capa más baja, que son los manejadores: collections
+
+
+
+
---
+++ staff/anna-cuenca/frontend/app.19/utils/asyncDelay.js
@@ -0,0 +1,5 @@
+function asyncDelay(callback, seconds) {
+    setTimeout(callback, seconds * 1000)
+}
+
+// ejecuta la función callback después de un retraso de unos segundos
