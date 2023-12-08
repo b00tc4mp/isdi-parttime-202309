6436e1acf82bea518748f6724ca248c88840c53d
Anna Cuenca <acuencabo@gmail.com>
2023-12-08 09:16:41 +0100

    clean comments

--- staff/anna-cuenca/backend/api/utils/CSV.js
+++ staff/anna-cuenca/backend/api/utils/CSV.js
@@ -1,81 +1,9 @@
-// // herramienta para leer archivos en forma de js
-
-// const fs = require('fs')
-// // es un paquete pre-fabricado de node, que significa File System
-// // es una caja de herramientas que tiene entre otras cosas readFile, que te permite leer el archivo que tú le digas
-// // tiene que estar en la carpeta dnd ejecutamos el node
-
-// function loadAsObject(file, callback) {
-
-
-//     fs.readFile(file, 'utf8', (error, csv) => {
-//         if (error) {
-//             callback(error)
-//             return
-//         }
-
-//         const data = []
-
-//         const lines = csv.split('\n') //aquí tenemos la 1a linea (separame hasta el salto de línea). Detecta todas las líneas
-
-//         const fields = lines[0].split(',') //aquí separo los elementos de la línea, me guio por las comas
-//         // en la línea 0 tengo las cabeceras
-
-//         for (let i = 1; i < lines.length; i++) { //quiero recorrer todas las líneas de usuarios (empiezo por la 1)
-//             const line = lines[i] // me guardo la información de la línea en la que estoy en la constante line
-//             const values = line.split(',') // y los valores de la línea los guardo en la variable values
-//             const item = {} // aquí guardaré los datos de cada usuario
-
-//             for (let j in fields) { //la j es el índice en el array fields, itero en fields
-//                 // la primera iteración que llega es "item-567,Pin Ocho,pin@ocho.com,123" j = 0
-//                 const field = fields[j] // field == 'id' porque estoy recortado de la cabecera, el primer elemento const fields = lines[0].split(',')
-
-//                 item[field] = values[j] // item[id] == 'item-567' // recorro values, que tiene los recortes de cada línea (y empiezo por la 1)
-//                 //creo una propiedad en el objeto item, que se llama como el campo, que en este caso es id
-//             }
-
-//             data.push(item)
-//         }
-
-//         callback(null, data)
-//     })
-
-// }
-
-// function saveFromObject(file, data, callback) { // en este fichero, guárdame este objeto
-//     const fields = Object.keys(data[0]) // aquí tengo las cabecera
-
-//     let csv = fields.join() //tenemos la línea 0
-
-//     for (const item of data) {
-//         //me traigo cada item, y me traifo dato a dato los ddadtos que están y me los pone en data
-//         let line = ''
-
-//         for (let i = 0; i < fields.length; i++) {
-//             const field = fields[i]
-
-//             line += item[field] + (i < fields.lenght - 1 ? ',' : '')
-//         }
-
-//         csv += '\r\n' + line
-
-//     }
-//     fs.writeFile(file, csv, error => {
-//         if (error) {
-//             callback(error) // esto es así porque solo tengo un dato (el error) que le paso al callback
-//             // si tiene error que me lo devuelva, sino que sea null
-//         }
-//         callback(null)
-//     }) // ahor lo guardo en disco
-// }
-
-// module.exports = {
-//     loadAsObject,
-//     saveFromObject
-// }
-
 
 const fs = require('fs')
+
+// es un paquete pre-fabricado de node, que significa File System
+// es una caja de herramientas que tiene entre otras cosas readFile, que te permite leer el archivo que tú le digas
+// tiene que estar en la carpeta dnd ejecutamos el node
 
 function loadAsObject(file, callback) {
     fs.readFile(file, 'utf8', (error, csv) => {
@@ -87,21 +15,24 @@ function loadAsObject(file, callback) {
 
         const data = []
 
-        const lines = csv.split('\r\n')
+        const lines = csv.split('\r\n') //aquí tenemos la 1a linea (separame hasta el salto de línea). Detecta todas las líneas
 
-        const fields = lines[0].split(',')
+        const fields = lines[0].split(',') //aquí separo los elementos de la línea, me guio por las comas
+        // en la línea 0 tengo las cabeceras
 
-        for (let i = 1; i < lines.length; i++) {
-            const line = lines[i]
+        for (let i = 1; i < lines.length; i++) { //quiero recorrer todas las líneas de usuarios (empiezo por la 1)
+            const line = lines[i] // me guardo la información de la línea en la que estoy en la constante line
 
-            const values = line.split(',')
+            const values = line.split(',') // y los valores de la línea los guardo en la variable values
 
-            const item = {}
+            const item = {} // aquí guardaré los datos de cada usuario
 
-            for (const j in fields) {
-                const field = fields[j]
+            for (const j in fields) {  //la j es el índice en el array fields, itero en fields
+                const field = fields[j]  // la primera iteración que llega es "item-567,Pin Ocho,pin@ocho.com,123" j = 0
+                // field == 'id' porque estoy recortado de la cabecera, el primer elemento const fields = lines[0].split(',')
+                item[field] = values[j] // item[id] == 'item-567' // recorro values, que tiene los recortes de cada línea (y empiezo por la 1)
+                //creo una propiedad en el objeto item, que se llama como el campo, que en este caso es id
 
-                item[field] = values[j]
             }
 
             data.push(item)
@@ -111,12 +42,13 @@ function loadAsObject(file, callback) {
     })
 }
 
-function saveFromObject(file, data, callback) {
-    const fields = Object.keys(data[0])
+function saveFromObject(file, data, callback) { // en este fichero, guárdame este objeto
+    const fields = Object.keys(data[0]) // aquí tengo las cabecera
 
-    let csv = fields.join()
+    let csv = fields.join() //tenemos la línea 0
 
     for (const item of data) {
+        //me traigo cada item, y me traifo dato a dato los ddadtos que están y me los pone en data
         let line = ''
 
         for (let i = 0; i < fields.length; i++) {
@@ -130,7 +62,8 @@ function saveFromObject(file, data, callback) {
 
     fs.writeFile(file, csv, error => {
         if (error) {
-            callback(error)
+            callback(error) // esto es así porque solo tengo un dato (el error) que le paso al callback
+            // si tiene error que me lo devuelva, sino que sea null
 
             return
         }
