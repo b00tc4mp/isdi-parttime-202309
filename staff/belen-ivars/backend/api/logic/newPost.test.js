7e25fd338765a7d5be194a1c5cc1191cee1a3346
Belén Ivars <berlem@MacBook-Air-de-Belen.local>
2023-12-10 22:52:06 +0100

    implement new post functionality api #152

---
+++ staff/belen-ivars/backend/api/logic/newPost.test.js
@@ -0,0 +1,15 @@
+const newPost = require('./newPost')
+
+try {
+	newPost('206s36wbyvpc', 'http://www.lafaneca.net/images/virtuemart/product/resized/calabacines-451x300_0x200.jpg', 'hello!', error => {
+
+		if (error) {
+			console.error(error)
+
+			return
+		}
+		console.log('new post update')
+	})
+} catch (error) {
+	console.log(error)
+}
