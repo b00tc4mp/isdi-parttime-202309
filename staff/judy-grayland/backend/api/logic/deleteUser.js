6286089bd85282cc71bc8d5f184bd2c56cbea0b4
Judy Grayland <juditta99@protonmail.com>
2023-12-11 19:10:49 +0100

    implement delete user functionality in API

---
+++ staff/judy-grayland/backend/api/logic/deleteUser.js
@@ -0,0 +1,46 @@
+const CSV = require('../utils/CSV')
+const { validateText, validateFunction } = require('../utils/validators')
+
+function deleteUser(userId, password, callback) {
+  validateText(userId, 'user id')
+  validateText(password, 'password')
+  validateFunction(callback, 'callback')
+
+  CSV.loadAsObject('./data/users.csv', (error, users) => {
+    if (error) {
+      callback(error)
+
+      return
+    }
+
+    let user = users.find((user) => user.id === userId)
+
+    if (!user) {
+      callback(new Error('user not found'))
+
+      return
+    }
+
+    if (user.password !== password) {
+      callback(new Error('wrong credentials'))
+
+      return
+    }
+
+    const index = users.indexOf(user)
+
+    users.splice(index, 1)
+
+    CSV.saveFromObject('./data/users.csv', users, (error) => {
+      if (error) {
+        callback(error)
+
+        return
+      }
+
+      callback(null, user.id)
+    })
+  })
+}
+
+module.exports = deleteUser
--- staff/judy-grayland/backend/api/logic/registerUser.test.js
+++ staff/judy-grayland/backend/api/logic/registerUser.test.js
@@ -1,7 +1,7 @@
 const registerUser = require('./registerUser')
 
 try {
-  registerUser('Lechu Guita', 'lechu@guita', '123123123', (error) => {
+  registerUser('Cala Bacin', 'cala@bacin', '123123123', (error) => {
     if (error) {
       console.error(error)
 
