e7f01d23343c6d8b7818105b91f92d67a0be6ddd
Manuel Barzi <manuelbarzi@gmail.com>
2023-12-14 21:36:45 +0100

    add create post and toggle like post logic and enpoints, doc, test; add custom errors #111

--- staff/manuel-barzi/backend/api/README.md
+++ staff/manuel-barzi/backend/api/README.md
@@ -4,7 +4,7 @@
 
 Request: POST /users "Content-Type: application/json" { name, email, password }
 Response: 201
-Response (error): 400 "Content-Type: application/json" { error, message }
+Response (error): 400|409|500 "Content-Type: application/json" { error, message }
 
 ## Authenticate user
 
@@ -17,3 +17,15 @@ Response (error): 400 "Content-Type: application/json" { error, message }
 Request: GET /users "Authorization: Bearer userId"
 Response: 200 "Content-Type: application/json" { name }
 Response (error): 400 "Content-Type: application/json" { error, message }
+
+## Create post
+
+Request: POST /posts "Authorization: Bearer userId" "Content-type: application/json" { image, text }
+Response: 201
+Response (error): 400 "Content-Type: application/json" { error, message }
+
+## Toggle like post
+
+Request: PATCH /posts/postId/likes "Authorization: Bearer userId"
+Response: 204
+Response (error): 400|404|406|500 "Content-Type: application/json" { error, message }
--- staff/manuel-barzi/backend/api/index.js
+++ staff/manuel-barzi/backend/api/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const registerUser = require('./logic/registerUser')
 const authenticateUser = require('./logic/authenticateUser')
 const retrieveUser = require('./logic/retrieveUser')
+const createPost = require('./logic/createPost')
+const toggleLikePost = require('./logic/toggleLikePost')
+const { SystemError, NotFoundError, ContentError, DuplicityError } = require('./utils/errors')
 
 const server = express()
 
@@ -15,7 +18,14 @@ server.post('/users', jsonBodyParser, (req, res) => {
 
         registerUser(name, email, password, error => {
             if (error) {
-                res.status(400).json({ error: error.constructor.name, message: error.message })
+                let status = 400
+
+                if (error instanceof SystemError)
+                    status = 500
+                else if (error instanceof DuplicityError)
+                    status = 409
+
+                res.status(status).json({ error: error.constructor.name, message: error.message })
 
                 return
             }
@@ -23,7 +33,12 @@ server.post('/users', jsonBodyParser, (req, res) => {
             res.status(201).send()
         })
     } catch (error) {
-        res.status(400).json({ error: error.constructor.name, message: error.message })
+        let status = 400
+
+        if (error instanceof ContentError)
+            status = 406
+
+        res.status(status).json({ error: error.constructor.name, message: error.message })
     }
 })
 
@@ -60,6 +75,58 @@ server.get('/users', (req, res) => {
         })
     } catch (error) {
         res.status(400).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
+server.post('/posts', jsonBodyParser, (req, res) => {
+    try {
+        const userId = req.headers.authorization.substring(7)
+
+        const { image, text } = req.body
+
+        createPost(userId, image, text, error => {
+            if (error) {
+                res.status(400).json({ error: error.constructor.name, message: error.message })
+
+                return
+            }
+
+            res.status(201).send()
+        })
+    } catch (error) {
+        res.status(400).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
+server.patch('/posts/:postId/likes', (req, res) => {
+    try {
+        const userId = req.headers.authorization.substring(7)
+
+        const { postId } = req.params
+
+        toggleLikePost(userId, postId, error => {
+            if (error) {
+                let status = 400
+
+                if (error instanceof SystemError)
+                    status = 500
+                else if (error instanceof NotFoundError)
+                    status = 404
+
+                res.status(status).json({ error: error.constructor.name, message: error.message })
+
+                return
+            }
+
+            res.status(204).send()
+        })
+    } catch (error) {
+        let status = 400
+
+        if (error instanceof ContentError)
+            status = 406
+
+        res.status(status).json({ error: error.constructor.name, message: error.message })
     }
 })
 
