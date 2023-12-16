dd7e2e136024bc2436560665b9dadf23fe3c2474
Your Name <anadelriocollar@ana.local>
2023-12-16 17:48:53 +0100

    integrate  authenticate and register user logic; refactor endpoints testinf (add testinf tool pepetest.sh #178

--- staff/ana-delrio/backend/api/README.md
+++ staff/ana-delrio/backend/api/README.md
@@ -2,23 +2,18 @@
 
 ## Register user
 
-```sh
-$ curl 'http://localhost:8000/register' \
--H 'Content-Type: application/json' \
--d '{ "name": "Man Zana", "email": "man@zana.com", "password": "123123123" }' \
--v
+Request: POST /users "Content-Type: application/json" { name, email, password }
+Response: 201
+Response (error): 400 "Content-Type: application/json" { error, message }
 
-> POST /register HTTP/1.1
-> Host: localhost:8000
-> User-Agent: curl/8.1.2
-> Accept: */*
-> Content-Type: application/json
-> Content-Length: 72
+## Authenticate user
 
-< HTTP/1.1 201 Created
-< X-Powered-By: Express
-< Date: Tue, 12 Dec 2023 20:30:46 GMT
-< Connection: keep-alive
-< Keep-Alive: timeout=5
-< Content-Length: 0
-```
+Request: POST /users/auth "Content-Type: application/json" { email, password }
+Response: 200 application/jason userId
+Response (error): 400 "Content-Type: application/json" { error, message }
+
+## Retrieve user 
+
+Request: GET /users "Authorization: Bearer userId" 
+Response: 200 "Content-Type: application/jason" { error, message }
+Response (error): 400 "Content-Type: application/json" { error, message }
--- staff/ana-delrio/backend/api/index.js
+++ staff/ana-delrio/backend/api/index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const registerUser = require('./logic/registerUser')
+const authenticateUser = require('./logic/authenticateUser')
+const retrieveUser = require('./logic/retrieveUser')
 
 const server = express()
 
 server.get('/', (req, res) => res.send('Hello, World!'))
 
-// middleware: es un software que esta en el medio entre la petición y la respuesta
-// TEST in browser GET http://localhost:8000/hello?name=Ana&surname=delRio
-server.get('/hello', (req, res) => res.send(`Hello, ${req.query.name} ${req.query.surname}!`))
+
 
 // es un middelware: te permite convertir cualquier peticion que le enviemos al servidor con un cuerpo json, lo convierte a objeto, en la propiedad body de la request
 const jsonBodyParser = express.json()
 
-server.post('/register', jsonBodyParser, (req, res) => {
+server.post('/users', jsonBodyParser, (req, res) => {
 
     try {
         const { name, email, password } = req.body
@@ -29,6 +29,49 @@ server.post('/register', jsonBodyParser, (req, res) => {
         res.status(400).json({ error: error.constructor.name, message: error.message })
 
     }
+})
+
+server.post('/users/auth', jsonBodyParser, (req, res) => {
+    try {
+        const name = { email, password } = req.body
+
+        authenticateUser(email, password, (error, userId) => {
+            if (error) {
+                res.status(400).json({ error: error.constructor.name, message: error.message })
+
+                return
+            }
+
+            res.json(userId)
+        })
+    } catch (error) {
+        res.status(400).json({ error: error.constructor.name, message: error.message })
+
+    }
+})
+
+server.get('/users', (req, res) => {
+    try {
+        const userId = req.headers.authorization.substring(7)
+
+        retrieveUser(userId, (error, user) => {
+            if (error) {
+                res.status(400).json({
+                    error: error.constructor.name, message: error.message
+                })
+
+                return
+            }
+            res.json(user)
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            error: error.constructor.name, message: error.message
+        })
+
+    }
+
 })
 
 server.listen(8000, () => console.log('server is up'))
