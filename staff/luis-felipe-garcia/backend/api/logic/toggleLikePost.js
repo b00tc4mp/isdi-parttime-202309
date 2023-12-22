ea9071fe026d0ae67313db3766197d66ef6fe942
Luis Felipe García <gpintolf@gmail.com>
2023-12-22 07:17:24 +0100

    #218 add toggleLikePost v1; need to code unlike functionality

--- staff/luis-felipe-garcia/backend/api/logic/toggleLikePost.js
+++ staff/luis-felipe-garcia/backend/api/logic/toggleLikePost.js
@@ -1,57 +1,81 @@
-const { validateText, validateFunction } = require("../utils/validators")
-const JSON = require('../utils/JSON')
-const {NotFoundError, SystemError} = require ("../utils/errors")
-
+const { validateId, validateFunction } = require("./helpers/validators")
+const { NotFoundError, SystemError } = require("./errors")
+const { User, Post } = require('../data/models')
 
 function toggleLikePost(userId, postId, callback) {
-    validateText(userId, 'user id')
-    validateText(postId, 'post id')
+    validateId(userId, 'user id')
+    validateId(postId, 'post id')
     validateFunction(callback, 'callback')
 
-    JSON.parseFromFile('./data/users.json', (error, users) => {
-        if (error) {
-            callback(new SystemError(error.message))
-            return
-        }
-
-        const user = users.find(user => user.id === userId)
-
-        if (!user) {
-            callback(new NotFoundError('user not found'))
-            return
-        }
-
-        JSON.parseFromFile('./data/posts.json', (error, posts) => {
-            if (error) {
-                callback(new SystemError(error.message))
-                return
-
-            }
-
-            const postIndex = posts.findIndex(post => post.id === postId)
-
-            if (postIndex < 0) {
-                callback(new NotFoundError('post not found'))
+    User.findById(userId)
+        .then(user => {
+            if (!user) {
+                callback(new NotFoundError('user id do not exist'))
                 return
             }
 
-            const post = posts[postIndex]
-            const userIdIndex = post.likes.indexOf(userId)
+            Post.findById(postId)
+                .then(post => {
+                    if (!post) {
+                        callback(new NotFoundError('post id do not exist'))
+                        return
+                    }
 
-            if (userIdIndex < 0)
-                post.likes.push(userId)
-            else post.likes.splice(userIdIndex, 1)
+                    post.likes.push(userId)
+                    post.save()
+                        .then(() => callback(null))
+                        .catch(error => callback(new SystemError(error.message)))
 
-            JSON.stringifyToFile('./data/posts.json', posts, error => {
-                if (error) {
-                    callback(new SystemError('error.message'))
-                    return
-                }
-                callback(null)
-            })
+                })
+                .catch(error => callback(new SystemError(error.message)))
 
         })
-    })
+        .catch(error => callback(new SystemError(error.message)))
+
+    // JSON.parseFromFile('./data/users.json', (error, users) => {
+    //     if (error) {
+    //         callback(new SystemError(error.message))
+    //         return
+    //     }
+
+    //     const user = users.find(user => user.id === userId)
+
+    //     if (!user) {
+    //         callback(new NotFoundError('user not found'))
+    //         return
+    //     }
+
+    //     JSON.parseFromFile('./data/posts.json', (error, posts) => {
+    //         if (error) {
+    //             callback(new SystemError(error.message))
+    //             return
+
+    //         }
+
+    //         const postIndex = posts.findIndex(post => post.id === postId)
+
+    //         if (postIndex < 0) {
+    //             callback(new NotFoundError('post not found'))
+    //             return
+    //         }
+
+    //         const post = posts[postIndex]
+    //         const userIdIndex = post.likes.indexOf(userId)
+
+    //         if (userIdIndex < 0)
+    //             post.likes.push(userId)
+    //         else post.likes.splice(userIdIndex, 1)
+
+    //         JSON.stringifyToFile('./data/posts.json', posts, error => {
+    //             if (error) {
+    //                 callback(new SystemError('error.message'))
+    //                 return
+    //             }
+    //             callback(null)
+    //         })
+
+    //     })
+    // })
 }
 
 module.exports = toggleLikePost
--- staff/luis-felipe-garcia/backend/api/logic/toggleLikePost.test.js
+++ staff/luis-felipe-garcia/backend/api/logic/toggleLikePost.test.js
@@ -1,16 +1,22 @@
-const toggleLikePost = require ('./toggleLikePost')
+const mongoose = require('mongoose')
+const toggleLikePost = require('./toggleLikePost')
 
-try {
-    toggleLikePost( "1g6al6ee05pc", "40ga0cl8bd00", error => {
-        if (error){
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+    .then(() => {
+        try {
+            toggleLikePost("6584656975fc0b52c39022e1", "658522cd96367f332bc1518b", error => {
+                if (error) {
+                    console.error(error)
+                    return
+                }
+
+                console.log('post like toggled')
+            })
+
+        } catch (error) {
             console.error(error)
-            return
-        }
 
-        console.log('post like toggled')
+        }
     })
-    
-} catch (error) {
-    console.error(error)
-    
-}
+
+    .catch(error => console.error(error))
