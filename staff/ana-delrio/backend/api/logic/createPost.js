952566be73f3a0bd4103c242640fc721b11bd632
Your Name <anadelriocollar@ana.local>
2023-12-29 11:43:49 +0100

    add some logics in api folder

--- staff/ana-delrio/backend/api/logic/createPost.js
+++ staff/ana-delrio/backend/api/logic/createPost.js
@@ -1,13 +1,26 @@
-const { validateText, validateFunction } = require('./helpers/validators')
+const { User, Post } = require('../data/models.js')
+const { NotFoundError, SystemError } = require('./errors.js')
+const { validateText, validateFunction, validateId } = require('./helpers/validators.js')
 
 
-function createPost(userId, image, text, callback) {
-    validateText(userId, 'user id')
+function createPost(author, image, text, callback) {
+    validateId(author, 'user id')
     validateText(image, 'image')
     validateText(text, 'text')
     validateFunction(callback, 'callback')
 
-    // TODO
+    User.findById(author)
+        .then(user => {
+            if (!user) {
+                callback(new NotFoundError('user not found'))
+                return
+            }
+
+            Post.create({ author, image, text })
+                .then(callback(null))
+                .catch(error => callback(new SystemError(error.message)))
+        })
+        .catch(error => callback(new SystemError(error.message)))
 }
 
 module.exports = createPost
