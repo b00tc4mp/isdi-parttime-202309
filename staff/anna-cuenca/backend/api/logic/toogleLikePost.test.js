b6787a2ecbc006c2d12ef98c63a9f676b0d706f8
Anna Cuenca <acuencabo@gmail.com>
2024-01-01 21:43:39 +0100

    add toogleLikePost logic #228

--- staff/anna-cuenca/backend/api/logic/toogleLikePost.test.js
+++ staff/anna-cuenca/backend/api/logic/toogleLikePost.test.js
@@ -1,16 +1,24 @@
+
+const mongoose = require('mongoose')
 const toogleLikePost = require('./toogleLikePost')
 
-try {
-    toogleLikePost("3vxs04m50tk0", "4mls4h86aba0", error => {
-        if (error) {
-            console.error(error)
-            return
-        }
+mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-        console.log('post like toogled')
+    .then(() => {
+
+        try {
+            toogleLikePost('658b1787d6c3f0f29677ccc6', '658b0eb28e3b8aedd4aeb966', error => {
+                if (error) {
+                    console.error(error)
+                    return
+                }
+
+                console.log('post like toogled')
+            })
+        } catch (error) {
+            console.error(error)
+        }
     })
 
-} catch (error) {
-    console.error(error)
-}
+    .catch(error => console.error(error))
 
