02e64fcb9f63c8bd997c3df6360bba436ffa40fd
Manuel Barzi <manuelbarzi@gmail.com>
2024-01-11 21:38:55 +0100

    refactor api handlers, validators; move settings to file in api; refactor app validators; move settings to file in app; #200

--- staff/manuel-barzi/fullstack/api/logic/createPost.js
+++ staff/manuel-barzi/fullstack/api/logic/createPost.js
@@ -1,12 +1,12 @@
-const { validateId, validateText, validateFunction } = require('./helpers/validators')
+const validate = require('./helpers/validate')
 const { User, Post } = require('../data/models')
 const { SystemError, NotFoundError } = require('./errors')
 
 function createPost(userId, image, text, callback) {
-    validateId(userId, 'user id')
-    validateText(image, 'image')
-    validateText(text, 'text')
-    validateFunction(callback, 'callback')
+    validate.id(userId, 'user id')
+    validate.text(image, 'image')
+    validate.text(text, 'text')
+    validate.function(callback, 'callback')
 
     User.findById(userId).lean()
         .then(user => {
--- staff/manuel-barzi/fullstack/api/logic/toggleFavPost.js
+++ staff/manuel-barzi/fullstack/api/logic/toggleFavPost.js
@@ -1,11 +1,11 @@
 const { NotFoundError, SystemError } = require("./errors")
-const { validateText, validateFunction } = require("./helpers/validators")
+const validate = require("./helpers/validate")
 const { User, Post } = require("../data/models")
 
 function toggleFavPost(userId, postId, callback) {
-    validateText(userId, 'user id')
-    validateText(postId, 'post id')
-    validateFunction(callback, 'callback')
+    validate.text(userId, 'user id')
+    validate.text(postId, 'post id')
+    validate.function(callback, 'callback')
 
     User.findById(userId)
         .then(user => {
