fbcf9ddad676559f4f9824d2b017167081721c23
Judy Grayland <juditta99@protonmail.com>
2024-01-16 13:49:00 +0100

    branch reset

--- staff/judy-grayland/fullstack/app/src/components/index.js
+++
@@ -1,8 +0,0 @@
-// A bucket in which to store all components
-
-import Post from './Post'
-import NewPost from './NewPost'
-import Profile from './Profile'
-import Posts from './Posts'
-
-export { Post, NewPost, Profile, Posts }
--- staff/judy-grayland/fullstack/app/src/library/Input.jsx
+++
@@ -1,5 +0,0 @@
-function Input (props) {
-    return <input className = "input" type={props.type || "text"} id={props.id}/>
-}
-
-export default Input
