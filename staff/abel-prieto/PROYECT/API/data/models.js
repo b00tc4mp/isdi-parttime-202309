08d48a090e812a7a294e58bc830b2e7ca4aadad6
Abel Prieto Martín <abelpriem94@hotmail.com>
2024-02-17 19:27:25 +0100

    create API models structure: [Guest-User-Root & Groups] #362

--- staff/abel-prieto/PROYECT/API/data/models.js
+++ staff/abel-prieto/PROYECT/API/data/models.js
@@ -1,24 +1,99 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 const { Schema, model, ObjectId } = mongoose
 
-const guess = new Schema({
-    
+// INVITADO
+const guest = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    group: [
+        {
+            type: String,
+            ref: 'Group',
+            default: 'guest'
+        }
+    ]
 })
 
+// USER
 const user = new Schema({
-
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    group: [
+        {
+            type: String,
+            ref: 'Group',
+            default: 'localhost'
+        }
+    ]
 })
 
+// ADMIN
 const admin = new Schema({
-
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    group: [
+        {
+            type: String,
+            ref: 'Group',
+            default: 'root'
+        }
+    ]
 })
 
-const Guess = new model('Guess', guess)
+// GROUP
+const group = new Schema({
+    guest: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    localhost: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    root: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+const Guest = new model('Guest', guest)
 const User = new model('User', user)
 const Admin = new model('Admin', admin)
 
-module.exports = {
-    Guess,
+const Group = new model('Group', group)
+
+export {
+    Guest,
     User,
-    Admin
+    Admin,
+    Group
 }
