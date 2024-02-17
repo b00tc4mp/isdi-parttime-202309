8604b5fe24b1ab297d258c8a1b581cf79f4a0a34
Tano Aleu-Mosteiro <tanoalmost@gmail.com>
2024-02-17 17:04:57 +0100

    move register, login and retrieve user in app to promises  #288

--- staff/tano-aleu/fullstack/app/src/logic/loginUser.js
+++ staff/tano-aleu/fullstack/app/src/logic/loginUser.js
@@ -1,11 +1,11 @@
 import { validate, errors } from 'com'
+const { SystemError } = errors
 
 import context from './context'
 
-function loginUser(email, password, callback) {
+function loginUser(email, password) {
     validate.email(email)
     validate.password(password)
-    validate.function(callback, 'callback')
 
     const req = {
         method: 'POST',
@@ -15,17 +15,17 @@ function loginUser(email, password, callback) {
         body: JSON.stringify({ email, password })
     }
 
-    fetch(`${import.meta.env.VITE_API_URL}/users/auth`, req)
+    return fetch(`${import.meta.env.VITE_API_URL}/users/auth`, req)
+        .catch(error => { throw new SystemError(error.message) })
         .then(res => {
             if (!res.ok) {
-                res.json()
-                    .then(body => callback(new errors[body.error](body.message)))
-                    .catch(error => callback(error))
-
-                return
+                return res.json()
+                    .catch(error => { throw new SystemError(error.message) })
+                    .then(body => { throw new errors[body.error](body.message) })
             }
 
-            res.json()
+            return res.json()
+                .catch(error => { throw new SystemError(error.message) })
                 .then(token => {
                     const payloadB64 = token.slice(token.indexOf('.') + 1, token.lastIndexOf('.'))
                     const payloadJson = atob(payloadB64)
@@ -34,12 +34,8 @@ function loginUser(email, password, callback) {
 
                     context.sessionUserId = userId
                     context.token = token
-
-                    callback(null)
                 })
-                .catch(error => callback(error))
         })
-        .catch(error => callback(error))
 }
 
 export default loginUser
