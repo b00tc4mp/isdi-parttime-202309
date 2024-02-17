42e832d3465d65d3566c974ab8340330215f8439
Anna Cuenca <acuencabo@gmail.com>
2024-02-18 00:56:27 +0100

    fix retrieveUser in app #381

--- staff/anna-cuenca/project/api/logic/index.js
+++ staff/anna-cuenca/project/api/logic/index.js
@@ -1,10 +1,12 @@
 import registerUser from './registerUser.js'
 import authenticateUser from './authenticateUser.js'
+import retrieveUser from './retrieveUser.js'
 //import arduinoController from './arduinoController.js'
 
 const logic = {
     registerUser,
-    authenticateUser
+    authenticateUser,
+    retrieveUser
 
 }
 
--- staff/anna-cuenca/project/api/test/retrieve-user.test.sh
+++ staff/anna-cuenca/project/api/test/retrieve-user.test.sh
@@ -5,5 +5,5 @@ TEST "retrieve-user"
 CASE "success on correct user id"
 
 curl 'http://localhost:9000/users' \
--H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWQwZjA5MjkwOGE5ZTcwZTk3YTI5ZDYiLCJpYXQiOjE3MDgyMDE0NzIsImV4cCI6MTcwODIwNTA3Mn0.EsOUWjtZC2eeGU0OhjD1JWMB0ayg4T5sVJMV53dPS0s' \
+-H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWQwZjA5MjkwOGE5ZTcwZTk3YTI5ZDYiLCJpYXQiOjE3MDgyMTM1OTYsImV4cCI6MTcwODIxNzE5Nn0.8j86ATQNjbg1UsOwWk8txS6qG-0OFUc0ff4QfFs9uwk' \
 -v
