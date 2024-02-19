ae37e6ebbab5e978cada45d619da1ef2aa92ff24
Tano Aleu-Mosteiro <tanoalmost@gmail.com>
2024-02-19 20:50:06 +0100

    move register, authenticate and retrieve user logics in app to async-await #288

--- staff/tano-aleu/fullstack/app/src/logic/registerUser.js
+++ staff/tano-aleu/fullstack/app/src/logic/registerUser.js
@@ -1,28 +1,38 @@
 import { validate, errors } from 'com'
 const { SystemError } = errors
 
-function registerUser(name, email, password) {
+export default function registerUser(name, email, password) {
     validate.text(name, 'name')
     validate.email(email)
     validate.password(password)
 
-    const req = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name, email, password })
-    }
+    return (async () => {
+        const req = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password })
+        }
 
-    return fetch(`${import.meta.env.VITE_API_URL}/users`, req)
-        .catch(error => { throw new SystemError(error.message) })
-        .then(res => {
-            if (!res.ok) {
-                return res.json()
-                    .catch(error => { throw new SystemError(error.message) })
-                    .then(body => { throw new errors[body.error](body.message) })
+        let res
+
+        try {
+            res = await fetch(`${import.meta.env.VITE_API_URL}/users`, req)
+        } catch (error) {
+            throw new SystemError(error.message)
+        }
+
+        if (!res.ok) {
+            let body
+
+            try {
+                body = await res.json()
+            } catch (error) {
+                throw new SystemError(error.message)
             }
-        })
-}
 
-export default registerUser
+            throw new errors[body.error](body.message)
+        }
+    })()
+}
