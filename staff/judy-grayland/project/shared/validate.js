79353800489713ce21341b60a7170a69f8ae4bb6
Judy Grayland <juditta99@protonmail.com>
2024-02-26 07:19:42 +0100

    add validate file #421

---
+++ staff/judy-grayland/project/shared/validate.js
@@ -0,0 +1,44 @@
+import { ContentError } from '../errors.js'
+
+const EMAIL_REGEX =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+// to ensure it's a mongoose id:
+const ID_REGEX = /^[0-9A-Fa-f]{24}$/
+
+function text(text, explain) {
+  if (typeof text !== 'string') throw new TypeError(explain + ' is not string')
+  if (!text.trim().length) throw new ContentError(explain + ' is empty')
+}
+
+function id(id, explain) {
+  text(id, explain)
+
+  if (!ID_REGEX.test(id)) throw new ContentError(`${explain} is not a valid id`)
+}
+
+function email(email, explain) {
+  text(email, explain)
+
+  if (!EMAIL_REGEX.test(email))
+    throw new ContentError(`The ${explain} provided is not a valid address`)
+}
+
+function number(number, explain) {
+  if (typeof number !== 'number')
+    throw new TypeError(`${explain} is not a number`)
+}
+
+function funktion(funktion, explain) {
+  if (typeof funktion !== 'function')
+    throw new TypeError(`${explain} is not a function`)
+}
+
+const validate = {
+  text,
+  id,
+  email,
+  number,
+  function: funktion,
+}
+
+export default validate
