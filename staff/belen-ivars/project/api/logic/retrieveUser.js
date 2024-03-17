4c856728bae32744282ca9ce47040264c0292070
Berlem <belen@ivars.me>
2024-03-17 23:57:49 +0100

    remove console logs

--- staff/belen-ivars/project/api/logic/retrieveUser.js
+++ staff/belen-ivars/project/api/logic/retrieveUser.js
@@ -5,12 +5,10 @@ async function retrieveUser(id) {
 
 	const user = await User.findById(id)
 
-	console.log(user)
-
 	if (!user) {
-		console.log("no user found")
 		throw new NotFoundError("No user found :cry:")
 	}
+
 	return user
 }
 
