86181cecb4a6c99688acbaa5601a61f851cf53b1
Tano Aleu-Mosteiro <tanoalmost@gmail.com>
2024-03-18 17:15:59 +0100

    implement retrieveFavSamples/toggleFavSample functionality and SampleFolder component #453

--- staff/tano-aleu/project/api/handlers/index.js
+++ staff/tano-aleu/project/api/handlers/index.js
@@ -2,12 +2,19 @@ import registerUserHandler from "./registerUserHandler.js";
 import authenticateUserHandler from "./authenticateUserHandler.js";
 import retrieveUserHandler from "./retrieveUserHandler.js";
 import getSamplesHandler from "./getSamplesHandler.js";
+import getMetronomoHandler from "./getMetronomoHandler.js";
+import toggleFavSampleHandler from "./toggleFavSampleHandler.js";
+import retrieveFavSamplesHandler from "./retrieveFavSamplesHandler.js";
 
 export {
     registerUserHandler,
     retrieveUserHandler,
     getSamplesHandler,
-    authenticateUserHandler
+    authenticateUserHandler,
+    getMetronomoHandler,
+    toggleFavSampleHandler,
+    retrieveFavSamplesHandler
+
 
 
 }
