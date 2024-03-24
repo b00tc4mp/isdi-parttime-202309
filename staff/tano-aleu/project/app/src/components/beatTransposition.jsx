8b47f07d6e10fc5df108db48dd481f7ddb143cd8
Tano Aleu-Mosteiro <tanoalmost@gmail.com>
2024-03-24 01:23:38 +0100

    css for components

--- staff/tano-aleu/project/app/src/components/beatTransposition.jsx
+++ staff/tano-aleu/project/app/src/components/beatTransposition.jsx
@@ -45,18 +45,24 @@ const BeatTransposition = ({ bpm, onBPMChange }) => {
         <div>
             <h3 className="flex justify-around">Beat Transposition</h3>
             <div onContextMenu={preventContextMenu} className="flex justify-around">
-                <button className="w-0 h-0 border-t-[15px] border-t-transparent border-r-[30px] border-r-purple-800 border-b-[15px] border-b-transparent" onMouseDown={() => startChangeTempo(-5)}
-                    onMouseUp={stopChangeTempo}
-                    onTouchStart={handleTouchStart(-5)}
-                    onTouchEnd={handleTouchEnd}
-                    onContextMenu={preventContextMenu}>
-                </button>
-                <button className="w-0 h-0 border-t-[15px] border-t-transparent border-l-[30px] border-l-purple-800 border-b-[15px] border-b-transparent" onMouseDown={() => startChangeTempo(5)}
-                    onMouseUp={stopChangeTempo}
-                    onTouchStart={handleTouchStart(5)}
-                    onTouchEnd={handleTouchEnd}
-                    onContextMenu={preventContextMenu}>
-                </button>
+                <div>
+                    <button className="w-0 h-0 border-t-[15px] border-t-transparent border-r-[30px] border-r-purple-800 border-b-[15px] border-b-transparent" onMouseDown={() => startChangeTempo(-5)}
+                        onMouseUp={stopChangeTempo}
+                        onTouchStart={handleTouchStart(-5)}
+                        onTouchEnd={handleTouchEnd}
+                        onContextMenu={preventContextMenu}>
+                    </button>
+                </div>
+
+                <div>
+                    <button className="w-0 h-0 border-t-[15px] border-t-transparent border-l-[30px] border-l-purple-800 border-b-[15px] border-b-transparent" onMouseDown={() => startChangeTempo(5)}
+                        onMouseUp={stopChangeTempo}
+                        onTouchStart={handleTouchStart(5)}
+                        onTouchEnd={handleTouchEnd}
+                        onContextMenu={preventContextMenu}>
+                    </button>
+                </div>
+
             </div>
         </div>
 
