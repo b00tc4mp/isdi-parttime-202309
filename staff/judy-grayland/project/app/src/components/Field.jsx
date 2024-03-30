c9bc9377b66160a984fb11fa8f630eb0a4d9f1c3
Judy Grayland <juditta99@protonmail.com>
2024-03-30 20:30:29 +0100

    add input type radio to Field component; create radio button form to select resource type  in Resource page #468

--- staff/judy-grayland/project/app/src/components/Field.jsx
+++ staff/judy-grayland/project/app/src/components/Field.jsx
@@ -1,8 +1,22 @@
 function Field(props) {
+  if (props.type === 'radio') {
+    return (
+      <>
+        <label htmlFor={props.inputId}>{props.children}</label>
+        <input
+          name={props.name}
+          type="radio"
+          id={props.inputId}
+          checked={props.checked}
+          onChange={props.onChange}
+        ></input>
+      </>
+    )
+  }
   return (
     <>
       <label htmlFor={props.inputId}>{props.children}</label>
-      <input name={props.name} id={props.inputId} />
+      <input name={props.name} type={props.type ?? 'text'} id={props.inputId} />
     </>
   )
 }
--- staff/judy-grayland/project/app/src/pages/Resources.jsx
+++ staff/judy-grayland/project/app/src/pages/Resources.jsx
@@ -1,7 +1,90 @@
-export default function Resources() {
+import { Form, Field, Button } from '../components'
+import { useState } from 'react'
+function handleSubmit(event) {
+  event.preventDefault()
+
+  console.log('resource created')
+}
+
+function Resources() {
+  const [resourceType, setResourceType] = useState('')
+
   return (
-    <div>
-      <h3>Resource</h3>
-    </div>
+    <>
+      <h2>Recursos</h2>
+      <h3>Añade un recurso</h3>
+      <Form onSubmit={handleSubmit}>
+        <div>
+          Elige el tipo de recurso:
+          <Field
+            name="resourceType"
+            type="radio"
+            inputId="activity-radio"
+            checked={resourceType === 'activity'}
+            onChange={() => setResourceType('activity')}
+          >
+            Actividad
+          </Field>
+          <Field
+            name="resourceType"
+            type="radio"
+            inputId="book-radio"
+            checked={resourceType === 'book'}
+            onChange={() => setResourceType('book')}
+          >
+            Libro
+          </Field>
+          <Field
+            name="resourceType"
+            type="radio"
+            inputId="special-date-radio"
+            checked={resourceType === 'special-date'}
+            onChange={() => setResourceType('special-date')}
+          >
+            Fecha especial
+          </Field>
+        </div>
+        {/* show different forms depending on what resource you choose */}
+        {resourceType === 'activity' && (
+          <div>
+            <Field name="title" inputId="title-input">
+              Título
+            </Field>
+            <Field name="image" inputId="image-input">
+              Imagen
+            </Field>
+            <Field name="description" inputId="description-input">
+              Descripción
+            </Field>
+            <Field name="link" inputId="link-input">
+              Enlace
+            </Field>
+            <Button>Submit</Button>
+          </div>
+        )}
+        {resourceType === 'book' && (
+          <div>
+            <Field name="title" inputId="title-input">
+              Título
+            </Field>
+            <Field name="author" inputId="author-input">
+              Autor
+            </Field>
+            <Field name="image" inputId="image-input">
+              Imagen
+            </Field>
+            <Field name="description" inputId="description-input">
+              Descripción
+            </Field>
+            <Field name="link" inputId="link-input">
+              Enlace
+            </Field>
+            <Button>Submit</Button>
+          </div>
+        )}
+      </Form>
+    </>
   )
 }
+
+export default Resources
