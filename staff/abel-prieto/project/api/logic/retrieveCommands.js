65ff4b4230c28ebbd000de09610659ea9057dfa7
Abel <abelpriem94@hotmail.com>
2024-04-06 12:24:53 +0200

    add some modifications on folders and rename project & branch

---
+++ staff/abel-prieto/project/api/logic/retrieveCommands.js
@@ -0,0 +1,19 @@
+import { User, Command } from '../data/models.js'
+import { errors, validate } from 'com'
+const { SystemError } = errors
+
+function retrieveCommands(userId) {
+    validate.id(userId, 'ID user')
+
+    return User.findById(userId).lean()
+        .catch(error => { throw new SystemError(error.message) })
+        .then(user => {
+            if (!user) {
+                return Command.find({ name: { $in: ['help', 'exit', 'login', 'register'] } }).lean().distinct('name')
+            }
+
+            return Command.find().lean().distinct('name')
+        })
+}
+
+export default retrieveCommands
