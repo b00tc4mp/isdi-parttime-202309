c2b2ba29c3af05d6299523f8b1e411a3b78ca37d
Isabel Domínguez Sánchez <134840727+IsaDScmd@users.noreply.github.com>
2024-04-07 20:13:30 +0200

    delete folder

--- staff/isabel-dominguez/frontend/app.15-appBIEN/utils/asyncDelay.js
+++
@@ -1,5 +0,0 @@
-function asyncDelay(callback, seconds) {
-    setTimeout(callback, seconds * 1000)
-}
-
-// Las tareas pueden ejecutarse simultáneamente, sin necesidad de esperar a que una tarea termine para comenzar otra. Esto permite que el programa continúe ejecutando otras tareas mientras se realizan operaciones que podrían tomar tiempo, como el acceso a base de datos o la obtención de datos desde internet.
--- staff/isabel-dominguez/frontend/app.15-appBIEN/utils/syncDelay.js
+++
@@ -1,9 +0,0 @@
-function syncDelay(callback, seconds) {
-    const before = Date.now()
-
-    while (Date.now() - before < seconds * 1000);
-
-    callback()
-}
-
-// Las tareas se ejecutan una tras otra, de manera secuencial. Cada tarea debe esperar a que la anterior se complete antes de comenzar.
