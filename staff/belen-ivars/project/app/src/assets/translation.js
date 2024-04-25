8288b03426799763002fa4eae78f8ce43d8319a2
Berlem <belen@ivars.me>
2024-04-25 20:24:27 +0200

    translation ca-en for work with inputs #407

---
+++ staff/belen-ivars/project/app/src/assets/translation.js
@@ -0,0 +1,10 @@
+export const dietTranslations = { 'glutenfree': 'glutenfree', 'vegan': 'vegà', 'vegetarian': 'vegetarià' }
+export const complexityTranslations = { 'easy': 'fàcil', 'regular': 'regular', 'complex': 'complexe' }
+export const methodTranslations = { 'steamed': 'al vapor', 'oven': 'forn', 'microwave': 'microones', 'grill': 'planxa', 'fresh': 'fresc', 'cook': 'coure' }
+
+export const getEnglishKey = (translationsList, stringToTranslate) => {
+	for (let key in translationsList) {
+		if (translationsList[key] === stringToTranslate) return key
+	}
+	throw new Error('no translation')
+}
