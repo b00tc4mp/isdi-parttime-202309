4d960617198028feea2405451158df954a7028f8
Judy Grayland <juditta99@protonmail.com>
2024-05-01 08:41:19 +0200

    add displayResources function and useEffect so all resources are loaded  when you first render the ResourcesList page #482ç

--- staff/judy-grayland/project/app/src/pages/ResourcesList.jsx
+++ staff/judy-grayland/project/app/src/pages/ResourcesList.jsx
@@ -1,17 +1,41 @@
 import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import { Button } from '../components'
-import Resource from './Resource'
+import Resource from './ResourceActivity'
+
+import logic from '../logic'
 
 function ResourcesList() {
   const navigate = useNavigate()
+  const [resources, setResources] = useState([])
 
   function handleNewResourceClick() {
     navigate('/resources/new')
   }
+
+  const displayResources = () => {
+    try {
+      logic
+        .retrieveResources()
+        .then((resources) => {
+          setResources(resources)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(() => {
+    displayResources()
+  }, [])
+
   return (
     <>
       <h2>Resources</h2>
+      <p>{resources.length}</p>
       <Resource></Resource>
       <Resource></Resource>
       <Resource></Resource>
