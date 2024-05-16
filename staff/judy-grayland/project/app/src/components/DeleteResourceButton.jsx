07dae09ca9e4c41020e463799a31ae5953216da6
Judy Grayland <juditta99@protonmail.com>
2024-05-16 13:12:58 +0200

    use useNavigate to reload page after resource is deleted #483

--- staff/judy-grayland/project/app/src/components/DeleteResourceButton.jsx
+++ staff/judy-grayland/project/app/src/components/DeleteResourceButton.jsx
@@ -2,14 +2,20 @@ import Button from './Button'
 import { NotFoundError } from '../../../shared/errors'
 import logic from '../logic'
 import { errors } from '../../../shared'
+import { useNavigate } from 'react-router-dom'
 
 function DeleteResourceButton({ resourceId }) {
+  const navigate = useNavigate()
   const handleDeleteResourceClick = () => {
     if (confirm('¿Seguro que quieres eliminar este recurso?')) {
       try {
         logic
           .deleteResource(resourceId)
-          .then(() => console.log('resource deleted sucessfully'))
+          .then(() => {
+            console.log('resource deleted sucessfully')
+            // by using navigate(0) we reload the page we're on, so the resource that's been deleted no longer shows
+            navigate(0)
+          })
           .catch((error) => {
             throw new Error(error)
           })
